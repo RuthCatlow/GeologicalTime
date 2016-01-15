33d12dc5b14985d713728d88cfa4e039d88958e5
garethfoote <gareth.foote@gmail.com>
2016-01-15 08:51:06 +0000

    Updated clear

--- clear.sh
+++ clear.sh
@@ -9,6 +9,8 @@ then
   cp /dev/null munge/munge.log
   cp /dev/null munge/ftp.log
   rm collection/{cams,log}.txt
-  rm output/{images,videos,write,tmp}/*
+  rm output/{images,videos}/*
+  rm output/{write,tmp}/ -r
+  rm output/count.json
   ssh ruth@gtp.ruthcatlow.net "rm ${GTP_BASE_DIR}{images,videos}/*; rm ${GTP_BASE_DIR}count.json"
 fi
