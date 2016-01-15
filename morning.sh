7444be68fd4e9c34ad59c56b38d502a28ab018e8
garethfoote <gareth.foote@gmail.com>
2016-01-15 09:17:36 +0000

    More morning scripting

--- morning.sh
+++ morning.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-ROOT=/home/gareth/Projects/GEOLOGICALTIME/
-# ROOT=/home/furtherfield/Desktop/GeologicalTime/
+echo $USER
+if [ "$USER" = "gareth" ]
+then
+  ROOT=/home/gareth/Projects/GEOLOGICALTIME/
+  PROCESSING=/home/gareth/Dropbox/Dev/processing/processing-3.0b2/processing
+else
+  ROOT=/home/furtherfield/Desktop/GeologicalTime/
+  PROCESSING=/home/gareth/Dropbox/Dev/processing/processing-3.0b2/processing
+fi
+
 rm ${ROOT}output/tmp/* -i
 rm ${ROOT}output/count.json -i
-
 cd $ROOT & git stash & git pull origin master
+
+${processing} ${ROOT}collection/collection.pde
+
