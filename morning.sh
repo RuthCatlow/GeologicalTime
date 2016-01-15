e82b71c751a29c1cd3c870c3be219343e953a0c3
garethfoote <gareth.foote@gmail.com>
2016-01-15 09:38:47 +0000

    Morning

--- morning.sh
+++ morning.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-echo $USER
 if [ "$USER" = "gareth" ]
 then
   ROOT=/home/gareth/Projects/GEOLOGICALTIME/
   PROCESSING=/home/gareth/Dropbox/Dev/processing/processing-3.0b2/processing
 else
   ROOT=/home/furtherfield/Desktop/GeologicalTime/
-  PROCESSING=/home/gareth/Desktop/processing-3.0.1/processing
+  PROCESSING=/home/furtherfield/Desktop/processing-3.0.1/processing
 fi
 
 rm ${ROOT}output/tmp/* -if
