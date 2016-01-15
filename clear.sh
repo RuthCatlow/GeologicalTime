6cd2c2fb4ffabd704780c4dede5a76424e4c5616
garethfoote <gareth.foote@gmail.com>
2016-01-15 08:30:34 +0000

    Adding clear script

---
+++ clear.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source ./munge/.env
+
+read -p "DANGER! You are about to remove all images and videos. Are you sure? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  cp /dev/null munge/munge.log
+  cp /dev/null munge/ftp.log
+  rm collection/{cams,log}.txt
+  rm output/{images,videos,write,tmp}/*
+  ssh ruth@gtp.ruthcatlow.net "rm ${GTP_BASE_DIR}{images,videos}/*; rm ${GTP_BASE_DIR}count.json"
+fi
