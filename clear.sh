dc63586ab71653719587bf9110c832e4229a4033
garethfoote <gareth.foote@gmail.com>
2016-01-27 16:52:36 +0000

    Safer clearing remotely

--- clear.sh
+++ clear.sh
@@ -12,10 +12,11 @@ fi
 cp /dev/null ${ROOT}munge/munge.log
 cp /dev/null ${ROOT}munge/ftp.log
 rm ${ROOT}collection/{cams,log}.txt
-ARCHIVE=${ROOT}archive$(date +%Y%m%d%H%M)
+DATETIME=$(date +%Y%m%d%H%M)
+ARCHIVE=${ROOT}archive${DATETIME}
 mkdir $ARCHIVE -p
 mv ${ROOT}output/{write,images,videos} $ARCHIVE
 rm ${ROOT}output/tmp/ -r
 rm ${ROOT}output/count.json
 mkdir ${ROOT}output/{images,videos}
-ssh ruth@gtp.ruthcatlow.net "mkdir ${GTP_BASE_DIR}archive/ -p; mv ${GTP_BASE_DIR}{images,videos} ${GTP_BASE_DIR}archive/; mv ${GTP_BASE_DIR}count.json ${GTP_BASE_DIR}archive/; mkdir ${GTP_BASE_DIR}{images,videos}"
+ssh ruth@gtp.ruthcatlow.net "mkdir ${GTP_BASE_DIR}archive${DATETIME}/ -p; mv ${GTP_BASE_DIR}{images,videos} ${GTP_BASE_DIR}archive${DATETIME}/; mv ${GTP_BASE_DIR}count.json ${GTP_BASE_DIR}archive${DATETIME}/; mkdir ${GTP_BASE_DIR}{images,videos}"
