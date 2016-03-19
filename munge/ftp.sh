ed6a43160918ba51f9cc68b2bb3d07cda3fef1bc
garethfoote <gareth.foote@gmail.com>
2016-03-19 14:55:51 +0000

    ftp script runs independantly

--- munge/ftp.sh
+++ munge/ftp.sh
@@ -9,6 +9,7 @@ var program = require('commander');
 var path = require('path');
 var winston = require('winston');
 var ssh2 = require("ssh2");
+var http = require("http");
 
 var mailOptions = {
 	to: 'gareth.foote@gmail.com',
@@ -30,8 +31,13 @@ program
   .option('-o, --output [path]', 'Change the output directory', __dirname+'/../output')
   .parse(process.argv);
 
-function ftpSend(){
+function ftpSend(count){
   winston.log('info', '<<<<<<<<<<<< SFTP client connecting >>>>>>>>>>>>');
+
+  var image = program.output + "/tmp/out"+pad('00000', count)+'.png'
+  var video = program.output + "/videos/video-"+pad('00000', count)+'.mp4'
+  console.log(image);
+  console.log(video);
 
   var options = {
     host: process.env.GTP_HOST,
@@ -40,8 +46,8 @@ function ftpSend(){
   };
   var baseDirectory = process.env.GTP_BASE_DIR;
   var files = [
-    { src: program.video, dest: baseDirectory+'videos/'+path.basename(program.video) },
-    { src: program.image, dest: baseDirectory+'images/'+path.basename(program.image) },
+    { src: video, dest: baseDirectory+'videos/'+path.basename(video) },
+    { src: image, dest: baseDirectory+'images/'+path.basename(image) },
     { src: program.output+'/count.json', dest: baseDirectory+'count.json' }
   ];
   var count = 0;
@@ -64,7 +70,7 @@ function ftpSend(){
           }
           winston.log('info', 'Uploaded: ' + file.src);
           if(count === files.length){
-            fs.unlinkSync(program.image, function(){
+            fs.unlinkSync(image, function(){
               winston.info('Image deleted');
             });
             c.end();
@@ -84,4 +90,48 @@ function logError(file, err){
   throw err;
 }
 
-ftpSend();
+var url = 'http://gtp.ruthcatlow.net/test/count.json?_cb='+(new Date()).getTime();
+
+http.get(url, function(res){
+  var body = '';
+
+  res.on('data', function(chunk){
+    body += chunk;
+  });
+
+  res.on('end', function(){
+    var response = JSON.parse(body);
+    console.log("Got a response: ", response.count);
+
+    var re = /0*([1-9][0-9]*|0)/;
+    var videoDirList = fileList(program.output+'/videos').reverse();
+    // Not the most recent because it could still be encoding.
+    var match = videoDirList[1].match(re);
+    if(match[1] > response.count){
+      // console.log('upload');
+      // console.log(match[1]);
+      ftpSend(match[1]);
+    } else {
+      console.log('do not upload');
+    }
+
+  });
+}).on('error', function(e){
+  console.log("Got an error: ", e);
+});
+
+
+function fileList(dir) {
+  return fs.readdirSync(dir).reduce(function(list, file) {
+    var name = [dir, file].join('/');
+		if(['png', 'jpg', 'jpeg', 'mp4'].indexOf(file.split('.').pop().toLowerCase()) === -1){
+			return list;
+		}
+    var isDir = fs.statSync(name).isDirectory();
+    return list.concat(isDir ? fileList(name) : [name]);
+  }, []);
+}
+
+function pad(pad, str) {
+  return (pad + str).slice(-pad.length);
+}
